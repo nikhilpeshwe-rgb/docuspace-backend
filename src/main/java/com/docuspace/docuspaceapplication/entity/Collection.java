5d24d31366e7bfe077871ee513a31cb35c32977d
nikhil.peshwe <nikhilpeshwe@gmail.com>
2026-04-10 04:09:41 -0500

    added key to collection entity

--- src/main/java/com/docuspace/docuspaceapplication/entity/Collection.java
+++ src/main/java/com/docuspace/docuspaceapplication/entity/Collection.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 public class Collection {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
